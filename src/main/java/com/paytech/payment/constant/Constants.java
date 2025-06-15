09c98bc30e2ecdf65c0fba4b2aa0a9bef14c93ac
Ianis Foris <ianis.foris@sportserve.co>
2025-06-15 18:25:08 +0300

    3rd

--- src/main/java/com/paytech/payment/constant/Constants.java
+++ src/main/java/com/paytech/payment/constant/Constants.java
@@ -1,4 +1,13 @@
 package com.paytech.payment.constant;
 
-public class Constants {
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class Constants {
+
+	public static final String REDIRECT_PAYMENT = "redirect:/payment";
+	public static final String PAYMENT_FORM = "payment-form";
+	public static final String REDIRECT = "redirect:";
 }
--- src/test/java/com/paytech/payment/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/com/paytech/payment/exception/GlobalExceptionHandlerTest.java
@@ -2,6 +2,9 @@ package com.paytech.payment.exception;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
@@ -9,32 +12,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
+@ExtendWith(MockitoExtension.class)
 class GlobalExceptionHandlerTest {
 
     private GlobalExceptionHandler handler;
+    @Mock
     private Model model;
 
     @BeforeEach
     void setUp() {
         handler = new GlobalExceptionHandler();
-        model = mock(Model.class);
     }
 
     @Test
-    void handlePaymentException_ReturnsPaymentErrorView() {
-        // Given
+    void handle_PaymentException_returns_payment_error_view() {
         PaymentException exception = new PaymentException("API error", "response body");
-
-        // When
         String viewName = handler.handlePaymentException(exception, model);
-
-        // Then
         assertEquals("payment-error", viewName);
     }
 
     @Test
     void handleWebClientException_ReturnsPaymentErrorView() {
-        // Given
         WebClientResponseException exception = WebClientResponseException.create(
             BAD_REQUEST.value(),
             "Bad Request",
@@ -42,23 +40,16 @@ class GlobalExceptionHandlerTest {
             "response body".getBytes(),
             null
         );
-
-        // When
         String viewName = handler.handleWebClientException(exception, model);
 
-        // Then
         assertEquals("payment-error", viewName);
     }
 
     @Test
-    void handleGenericException_ReturnsPaymentErrorView() {
-        // Given
+    void handle_GenericException_returns_payment_error_view() {
         Exception exception = new RuntimeException("Unexpected error");
-
-        // When
         String viewName = handler.handleGenericException(exception, model);
 
-        // Then
         assertEquals("payment-error", viewName);
     }
 }
